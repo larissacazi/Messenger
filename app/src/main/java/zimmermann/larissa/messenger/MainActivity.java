754ee383f35348cb03456d5ddc82083c6adb6e2c
Larissa Cardoso Zimmermann <larissacazi@hotmail.com>
2018-01-17 10:56:36 +0000

    Add Android Project

---
+++ app/src/main/java/zimmermann/larissa/messenger/MainActivity.java
@@ -0,0 +1,20 @@
+package zimmermann.larissa.messenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    //private DatabaseReference referenceFirebase = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //referenceFirebase.child("points").setValue(100);
+    }
+}
